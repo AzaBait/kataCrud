077a73c2ae0e5658874b4c65b29a350d48eef7ca
dell <m>
2022-08-31 12:25:02 +0600

    done

--- src/main/java/com/aza/service/UserServiceImpl.java
+++ src/main/java/com/aza/service/UserServiceImpl.java
@@ -3,37 +3,40 @@ package com.aza.service;
 import com.aza.dao.UserDao;
 import com.aza.model.User;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
-@Transactional
-public class UserServiceImpl implements UserService{
-   private UserDao userDao;
 
- public UserServiceImpl(UserDao userDao) {
-  this.userDao = userDao;
- }
+public class UserServiceImpl implements UserService {
+    private UserDao userDao;
 
- @Override
+    public UserServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Override
+    @Transactional
     public void save(User user) {
-    userDao.save(user);
+        userDao.save(user);
     }
 
     @Override
     public Object getById(Long id) {
-     return userDao.getById(id);
+        return userDao.getById(id);
     }
 
     @Override
+    @Transactional
     public void update(User user) {
-    userDao.update(user);
+        userDao.update(user);
     }
 
     @Override
+    @Transactional
     public void delete(Long id) {
-    userDao.delete(id);
+        userDao.delete(id);
     }
 
     @Override
